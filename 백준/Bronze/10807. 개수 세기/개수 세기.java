034e50d406371ce72967ac18893a1558562eaca2
seoyoon-riley <sylim0523@gmail.com>
2025-10-18 16:42:40 +0900

    Update 개수 세기 주석.java

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int intarray[] = new int[N];
-        int count = 0;
+        int N = sc.nextInt();  // N 입력받음
+        int intarray[] = new int[N];  // N개 길이의 intarray 생성
+        int count = 0;  // count 초기화
         for(int i = 0; i < N; i++) {
-            int n = sc.nextInt();
-            intarray[i] = n;
+            int n = sc.nextInt();  // N개의 정수를 공백을 구분으로 입력받겠다
+            intarray[i] = n;  // 입력된 정수 intarray에 저장
         }
-        int v = sc.nextInt();
+        int v = sc.nextInt();  // v입력
         for(int i = 0; i < N; i++) {
             if(intarray[i] == v)
-            count ++;
+            count ++;  // intarray의 원소가 v일때마다 count 1씩 증가
         }
         System.out.println(count);
         sc.close();
     }
 }
+
