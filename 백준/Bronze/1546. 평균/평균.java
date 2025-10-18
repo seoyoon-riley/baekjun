a3615467b5bec9604258540f0949ce7a142a78c3
seoyoon-riley <sylim0523@gmail.com>
2025-10-18 17:14:13 +0900

    Update 평균.java

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -5,7 +5,7 @@ public class Main {
 
         int N = sc.nextInt();
         int M = 0;
-        double sum = 0;
+        double sum = 0;   // double 처리 중요
         int intarray[] = new int[N];
         for(int i = 0; i < N; i++) {
             int score = sc.nextInt();
@@ -14,11 +14,12 @@ public class Main {
             M = score;  // 점수 중 최댓값 계속 갱신
         }
         for(int i = 0; i < N; i++) {
-            sum += (double)intarray[i]/M*100;  // 점수 갱신
+            sum += (double)intarray[i]/M*100;  // 점수 갱신, 정수 나눗셈이므로 double처리 중요!
         }
-        double newmean = (double)sum/N;
+        double newmean = (double)sum/N;  // 평균이 실수값이므로 double처리
         System.out.println(newmean);
         
         sc.close();
     }
 }
+
